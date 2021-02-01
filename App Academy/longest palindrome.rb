99b761a8500fa71ba6aed1a1efd9454be66dd27e
Michelle Lin <michelleamazinglin@gmail.com>
2021-02-01 00:33:25 -0500

    TC

--- App Academy/longest palindrome.rb
+++ App Academy/longest palindrome.rb
@@ -3,28 +3,30 @@
 # a given string. If there is no palindrome longer than two letters, return false.
 
 def longest_palindrome(string)
-    longest_palindrome = false
-    i = 0
-    while i < string.length - 1
-        j = i + 1
-        while j < string.length
-            current_str = string[i..j]
-            length = current_str.length
-            if is_palindrome?(current_str)
-                if !longest_palindrome || length > longest_palindrome
-                    longest_palindrome = length
+    longest_palindrome = false  #O(1)
+    i = 0                       #O(1)
+    while i < string.length - 1 #O(n)
+        j = i + 1               #O(1)
+        while j < string.length #O(n)
+            current_str = string[i..j]  #O(1)
+            length = current_str.length #O(1)
+            if is_palindrome?(current_str)  
+                if !longest_palindrome || length > longest_palindrome   #O(1)
+                    longest_palindrome = length #O(1)
                 end
             end
-            j += 1
+            j += 1  #O(1)
         end
-        i += 1
+        i += 1  #O(1)
     end
     longest_palindrome
 end
 def is_palindrome?(string)
-    string == string.reverse
+    string == string.reverse    #O(1)
 end
 # return false if no palindrom > 2
 # return length (integer)
+
+#O(n)
 
 puts longest_palindrome("ahsudjcbak")
---
+++ App Academy/symmetric substring.rb
@@ -0,0 +1,17 @@
+ # Define a method `String#symmetric_substrings` that returns an array of 
+  # substrings that are palindromes.  Only include substrings of length > 1.
+
+  # example: "cool".symmetric_substrings => ["oo"]
+
+  def symmetric_substrings(string)
+    arr = []
+    string.length.times do |start_pos|
+        (2..(string.length - start_pos)).to_a.each do |end_pos|
+            substr = string[start_pos...(start_pos + end_pos)]
+            arr << substr if substr == substr.reverse
+        end
+    end
+    arr
+  end
+
+  puts symmetric_substrings("hello")

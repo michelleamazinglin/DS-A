107ebe4d66f5c95975578a54740feb4b3721d3fd
Michelle Lin <michelleamazinglin@gmail.com>
2021-02-23 00:33:28 -0500

    my Flatten

---
+++ JS practice/myFlatten.js
@@ -0,0 +1,12 @@
+Array.prototype.myFlatten = function() {
+    let arr = []
+    for (let i = 0; i < this.length; i++) {
+        if (Array.isArray(this[i])) {
+            let ele = this[i].myFlatten();
+            arr.concat(ele);
+        } else {
+            arr.push(this[i])
+        }
+    }
+    return arr;
+}

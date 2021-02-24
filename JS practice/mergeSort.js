9f096fc11c5598e75940fe33b66d1444d133dc96
Michelle Lin <michelleamazinglin@gmail.com>
2021-02-24 03:47:30 -0500

    sort

--- JS practice/mergeSort.js
+++ JS practice/mergeSort.js
@@ -13,3 +13,43 @@
 // Split the array into left and right halves, then merge sort them recursively
 // until a base case is reached. Use a helper method, merge, to combine the
 // halves in sorted order, and return the merged array.
+
+const defaultCallback = function (num1, num2) {
+    if (num1 < num2) {
+        return -1;
+    } else if (num1 === num2) {
+        return 0;
+    } else {
+        return 1;
+    }
+};
+
+Array.prototype.mergeSort = function (callback) {
+    if (this.length <= 1) {return this};
+    if (typeof callback !== "function") {
+        callback = defaultCallback;
+    }
+    const mid = Math.floor(this.length / 2);
+    const left = this.slice(0, mid).mergeSort(callback);
+    const right = this.slice(mid).mergeSort(callback);
+    return merge(left,right,callback)
+}
+
+function merge(left, right, callback) {
+    let merge = [];
+    while (left.length && right.length) {
+        switch (callback(left[0], right[0])) {
+            case -1:
+                merge.push(left.shift());
+                break;
+            case 0:
+                merge.push(left.shift());
+                break;
+            case 1:
+                merge.push(right.shift());
+                break;
+        }
+    }
+    merge = merge.concat(left,right);
+    return merged;
+}
